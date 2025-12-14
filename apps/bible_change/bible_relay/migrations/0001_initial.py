4a30a656a4bcfdc935d6c19f78feb91342b15eed
jee1021 <dlawldnjs460@gmail.com>
2025-12-15 00:18:34 +0900

    성경댓글모델 작성(BIBLE-4)

---
+++ apps/bible_change/bible_relay/migrations/0001_initial.py
@@ -0,0 +1,89 @@
+from django.db import migrations, models
+import django.db.models.deletion
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='BibleRelayComment',
+            fields=[
+                (
+                    'id',
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name='ID'
+                    )
+                ),
+                (
+                    'book',
+                    models.CharField(
+                        max_length=50,
+                        verbose_name='성경 권'
+                    )
+                ),
+                (
+                    'chapter',
+                    models.PositiveIntegerField(
+                        verbose_name='장'
+                    )
+                ),
+                (
+                    'verse',
+                    models.PositiveIntegerField(
+                        verbose_name='절'
+                    )
+                ),
+                (
+                    'content',
+                    models.TextField(
+                        verbose_name='릴레이 댓글 내용'
+                    )
+                ),
+                (
+                    'is_completed',
+                    models.BooleanField(
+                        default=False,
+                        verbose_name='릴레이 완료 여부'
+                    )
+                ),
+                (
+                    'created_at',
+                    models.DateTimeField(
+                        auto_now_add=True,
+                        verbose_name='작성일'
+                    )
+                ),
+                (
+                    'updated_at',
+                    models.DateTimeField(
+                        auto_now=True,
+                        verbose_name='수정일'
+                    )
+                ),
+                (
+                    'user',
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name='bible_relay_comments',
+                        to=settings.AUTH_USER_MODEL,
+                        verbose_name='작성자'
+                    )
+                ),
+            ],
+            options={
+                'verbose_name': '성경 릴레이 댓글',
+                'verbose_name_plural': '성경 릴레이 댓글들',
+                'ordering': ['-created_at'],
+            },
+        ),
+    ]
